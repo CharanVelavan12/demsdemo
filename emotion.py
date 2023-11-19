1a847d3b5b6cfe4774d6e641449778359390c6e6
CharanVelavan12 <151447379+CharanVelavan12@users.noreply.github.com>
2023-11-19 22:00:18 +0530

    Update emotion.py

--- emotion.py
+++ emotion.py
@@ -1,14 +1,23 @@
 import streamlit as st
 
+# Replace these credentials with your own
+USERNAME = 'your_username'
+PASSWORD = 'your_password'
+
+def login():
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+
+    if st.button("Login") and username == USERNAME and password == PASSWORD:
+        st.success("Logged in successfully!")
+        return True
+    return False
+
 def main():
-    st.title("Simple Streamlit App")
+    st.title("Login Page")
+    if login():
+        st.text("Redirecting to Graph Page...")
+        st.experimental_run_as_function('graph_app.py')
 
-    # Text input
-    user_input = st.text_input("Enter your name:", "Type here...")
-
-    # Button
-    if st.button("Submit"):
-        st.success(f"Hello, {user_input}!")
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
