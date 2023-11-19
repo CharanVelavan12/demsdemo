01a7d01081e07143978d66c0efe095bcd029088a
CharanVelavan12 <151447379+CharanVelavan12@users.noreply.github.com>
2023-11-19 21:59:50 +0530

    graph.py

--- graph.py
+++ graph.py
@@ -1,41 +1,27 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_bcrypt import Bcrypt
+import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-from io import BytesIO
-import base64
+import plotly.express as px
 
-app = Flask(__name__)
-bcrypt = Bcrypt(app)
+@st.cache_data
+def create_line_graph(df):
+    fig = px.line(df, x='Timestamp', y='Emotion', markers=True, title='Emotion vs Timestamp')
+    return fig
 
-# Replace these credentials with your own
-USERNAME = 'charan'
-PASSWORD_HASH = bcrypt.generate_password_hash('123').decode('utf-8')
+@st.cache_data
+def create_bar_graph(df):
+    emotion_counts = df['Emotion'].value_counts().reset_index()
+    emotion_counts.columns = ['Emotion', 'Count']
 
-# Flag to check if the user is logged in
-logged_in = False
+    fig = px.bar(emotion_counts, x='Emotion', y='Count', title='Emotion Counts vs Timestamp',
+                 labels={'Emotion': 'Emotion', 'Count': 'Count'})
+    
+    dominant_emotion = emotion_counts['Emotion'].iloc[0]  # Assuming you want the emotion with the highest count
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    global logged_in
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    return fig, dominant_emotion
 
-        if username == USERNAME and bcrypt.check_password_hash(PASSWORD_HASH, password):
-            logged_in = True
-            return redirect(url_for('index'))
-        else:
-            return render_template('login.html', error='Invalid credentials')
-
-    return render_template('login.html', error=None)
-
-@app.route('/')
-def index():
-    global logged_in
-    if not logged_in:
-        return redirect(url_for('login'))
-
+def main():
+    st.title("Graph Page")
+    
     # Read CSV file
     df = pd.read_csv('emotion_data.csv')
 
@@ -45,44 +31,18 @@ def index():
     # Create bar graph
     bar_graph, dominant_emotion = create_bar_graph(df)
 
-    return render_template('index.html', line_graph=line_graph, bar_graph=bar_graph, dominant_emotion=dominant_emotion)
+    # Display graphs
+    st.markdown("## Emotion vs Timestamp")
+    st.plotly_chart(line_graph, use_container_width=True)
 
-def create_line_graph(df):
-    plt.figure(figsize=(10, 6))
-    plt.plot(df['Timestamp'], df['Emotion'], marker='o')
-    plt.title('Emotion vs Timestamp')
-    plt.xlabel('Timestamp')
-    plt.ylabel('Emotion')
-    plt.xticks(rotation=45)
+    st.markdown("## Emotion Counts vs Timestamp")
+    st.plotly_chart(bar_graph, use_container_width=True)
 
-    # Save the plot to a BytesIO object
-    img = BytesIO()
-    plt.savefig(img, format='png')
-    img.seek(0)
+    st.markdown(f"**Dominant Emotion:** {dominant_emotion}")
 
-    # Convert the plot to base64 for embedding in HTML
-    line_graph = base64.b64encode(img.getvalue()).decode()
+    # Display DataFrame
+    st.markdown("## Emotion Data Table")
+    st.dataframe(df)
 
-    return line_graph
-
-def create_bar_graph(df):
-    plt.figure(figsize=(10, 6))
-    emotion_counts = df['Emotion'].value_counts()
-    dominant_emotion = emotion_counts.idxmax()
-    plt.bar(emotion_counts.index, emotion_counts.values)
-    plt.title('Emotion Counts vs Timestamp')
-    plt.xlabel('Emotion')
-    plt.ylabel('Count')
-
-    # Save the plot to a BytesIO object
-    img = BytesIO()
-    plt.savefig(img, format='png')
-    img.seek(0)
-
-    # Convert the plot to base64 for embedding in HTML
-    bar_graph = base64.b64encode(img.getvalue()).decode()
-
-    return bar_graph, dominant_emotion
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    main()
